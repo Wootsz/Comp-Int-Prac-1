3b9376e74df71a1f4c8d40276acfd3e642ecc672
Wootsz <wouterbrozius@hotmail.com>
2017-06-21 12:33:37 +0200

    ooooooooo

--- Prac2/Program.cs
+++ Prac2/Program.cs
@@ -14,18 +14,23 @@ namespace Prac2
     {
         static int dims = 0, block_dims = 0;
         static List<int> values;
-        static int random_steps = 1;
-        static int plateau_steps = 3;
-        static List<Point> fixed_cells;
         static Dictionary<int, int[,]> local_optima;
+        static List<Point> fixed_cells, full_blocks;
         static Random random = new Random();
-        static int max_hill_iterations = 50;
+
+        static int random_steps = 10;
+        static int plateau_steps = 3;
+        static int max_hill_iterations = 100;
+        static int max_hill_iterations_o = max_hill_iterations;
+
+        static int hill_steps = 0, random_hill_steps = 0;
 
         static void Main(string[] args)
         {
-            //string puzzel1 = "0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0";
+            string puzzel1 = "0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0";
             string puzzel2 = "0 0 3 0 2 0 6 0 0 9 0 0 3 0 5 0 0 1 0 0 1 8 0 6 4 0 0 0 0 8 1 0 2 9 0 0 7 0 0 0 0 0 0 0 8 0 0 6 7 0 8 2 0 0 0 0 2 6 0 9 5 0 0 8 0 0 2 0 3 0 0 9 0 0 5 0 1 0 3 0 0";
-            string[] puzzel_array = puzzel2.Split();
+            string puzzel0 = "0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0";
+            string[] puzzel_array = puzzel0.Split();
 
             // Determine the puzzle dimensions based on the puzzel_array
             dims = (int)Math.Sqrt(puzzel_array.Length);
@@ -39,6 +44,7 @@ namespace Prac2
             // Fil an array (start_array) with values from the puzzel_array and the other needed values in the blocks
             int[,] start_state = new int[dims, dims];
             fixed_cells = new List<Point>();
+            full_blocks = new List<Point>();
             local_optima = new Dictionary<int, int[,]>();
             FillArray(start_state, puzzel_array);
             FillBlocks(start_state);
@@ -51,13 +57,14 @@ namespace Prac2
                 column_scores[i] = ColumnScore(start_state, i);
             }
 
-            //RandomWalkHillClimbing(start_state, row_scores, column_scores, 10);
-            Console.WriteLine("Finished");
-            WriteState(HillClimbing(start_state, row_scores, column_scores, -1, 0));
+            WriteState(start_state);
+            int[,] end = HillClimbing(start_state, row_scores, column_scores, -1, 0);
+            WriteState(end);
+            WriteStats(GetScore(row_scores, column_scores));
             Console.Read();
         }
 
-        static void WriteState(int[,] field)
+        static void WriteState(int[,] state)
         {
             for (int i = 0; i < dims; i++)
             {
@@ -67,11 +74,24 @@ namespace Prac2
                         Console.ForegroundColor = ConsoleColor.Red;
                     else
                         Console.ResetColor();
-                    Console.Write(field[i, j] + " ");
+                    Console.Write(state[i, j] + " ");
                 }
                 Console.WriteLine();
             }
             Console.WriteLine();
+        }
+
+        static void WriteStats(int score)
+        {
+            Console.WriteLine("Score: " + score);
+            Console.WriteLine();
+            Console.WriteLine("Max hill iterations: " + max_hill_iterations_o);
+            Console.WriteLine("Max random steps: " + random_steps);
+            Console.WriteLine("Max plateau steps: " + plateau_steps);
+            Console.WriteLine();
+            Console.WriteLine("Local search recursion times: " + hill_steps);
+            Console.WriteLine("Random search recursion times: " + random_hill_steps);
+            Console.WriteLine("Total recursion times: " + (hill_steps + random_hill_steps));
         }
 
         static void FillArray(int[,] state, string[] puzzel_array)
@@ -101,6 +121,11 @@ namespace Prac2
                         for (int j = y * block_dims; j < y * block_dims + block_dims; j++)
                             if (state[i, j] != 0)
                                 block_values.Remove(state[i, j]);
+
+                    // If there is only 1 free space (or 0) in the block, add it to the full_blocks list
+                    if (block_values.Count <= 1)
+                        full_blocks.Add(new Point(x, y));
+                    
                     // Then assign values to each cell in the block
                     for (int i = x * block_dims; i < x * block_dims + block_dims; i++)
                         for (int j = y * block_dims; j < y * block_dims + block_dims; j++)
@@ -149,10 +174,11 @@ namespace Prac2
 
         static int[,] HillClimbing(int[,] state, int[] row_scores, int[] column_scores, int prev_score, int n)
         {
-            //WriteState(state);
             if (max_hill_iterations == 0)
                 return state;
             max_hill_iterations--;
+
+            hill_steps++;
             
             int new_score = GetScore(row_scores, column_scores);
             // If new_score == 0, it means we have found the solution
@@ -165,15 +191,26 @@ namespace Prac2
             if (n >= plateau_steps)
             {
                 // If we are at the same local optimum again, stop the algorithm and return the state
+                //if (local_optima.Keys.Contains(new_score) && local_optima.Values.Contains(state))
+                //  return state;
                 if (local_optima.Keys.Contains(new_score))
-                    return state;
-                local_optima.Add(new_score, state);
-                return RandomWalkHillClimbing(state, row_scores, column_scores, random_steps);
+                    local_optima[new_score] = state;
+                else
+                    local_optima.Add(new_score, state);
+                int[,] best_optima = local_optima[local_optima.Keys.Max()];
+                return RandomWalkHillClimbing(best_optima, row_scores, column_scores, random_steps);
             }
-            Random random = new Random();
-            int random_x = random.Next(0, block_dims), random_y = random.Next(0, block_dims);
 
-            SortedDictionary<int, List<Point>> switches = new SortedDictionary<int, List<Point>>();
+            // Generate a random block coordinate that is not "full" (i.e. has 0 or 1 non-fixed cells)
+            int random_x, random_y;
+            do
+            {
+                random_x = random.Next(0, block_dims);
+                random_y = random.Next(0, block_dims);
+            }
+            while (full_blocks.Contains(new Point(random_x, random_y)));
+
+            Dictionary<int, List<Point>> switches = new Dictionary<int, List<Point>>();
 
             for (int i = random_x * block_dims; i < random_x * block_dims + block_dims; i++)
             {
@@ -212,22 +249,23 @@ namespace Prac2
 
                                 // Calculate the change in score
                                 int change_in_score = new_row_score1 + new_row_score2 + new_column_score1 + new_column_score2;
+
                                 // Add the switched points to the switches list
-                                if (switches.Keys.Contains(change_in_score))
+                                if (switches != null && switches.Keys.Contains(change_in_score))
                                 {
                                     switches[change_in_score].Add(new Point(i, j));
                                     switches[change_in_score].Add(new Point(k, l));
                                 }
                                 else
                                     switches.Add(change_in_score, new List<Point> { new Point(i, j), new Point(k, l) });
-
                             }
                         }
                     }
                 }
             }
+
             // Get the best switch
-            List<Point> best_switch = switches.First().Value;
+            List<Point> best_switch = switches[switches.Keys.Max()];//switches.First().Value;
 
             // If the new score is worse than the previous one, we are at an optimum, so go into recursion with n = n
             if (switches.First().Key > prev_score)
@@ -251,17 +289,23 @@ namespace Prac2
 
         static int[,] RandomWalkHillClimbing(int[,] state, int[] row_scores, int[] column_scores, int steps)
         {
-            Console.WriteLine("RANDOM");
-            //WriteState(state);
-
             // End condition
             if (steps <= 0)
                 return HillClimbing(state, row_scores, column_scores, GetScore(row_scores, column_scores), 0);
 
-            // Generate a random block coordinate
-            int random_x = random.Next(0, block_dims), random_y = random.Next(0, block_dims);
+            random_hill_steps++;
+
+            // Generate a random block coordinate that is not "full" (i.e. has 0 or 1 non-fixed cells)
+            int random_x, random_y;
+            do
+            {
+                random_x = random.Next(0, block_dims);
+                random_y = random.Next(0, block_dims);
+            }
+            while (full_blocks.Contains(new Point(random_x, random_y)));
+
             // Generate 4 random integers (the switch coordinates)
-            List<int> r = GetRandomCo(random_x, random_y, block_dims);
+            List<int> r = GetRandomCo(random_x, random_y);
             int i = r[0], j = r[1], k = r[2], l = r[3];
 
             // Do the switch with the two random coordinates values
@@ -278,14 +322,22 @@ namespace Prac2
             return RandomWalkHillClimbing(state, row_scores, column_scores, --steps);
         }
 
-        static List<int> GetRandomCo(int random_x, int random_y, int block_dims)
+        // Generate 4 integers / 2 coordinates that are not fixed or the same, and within a single block
+        static List<int> GetRandomCo(int random_x, int random_y)
         {
+            // Generate 4 random coordinates in the given block (random_x, random_y)
             int random_i = random.Next(random_x * block_dims, random_x * block_dims + block_dims);
             int random_j = random.Next(random_y * block_dims, random_y * block_dims + block_dims);
             int random_k = random.Next(random_x * block_dims, random_x * block_dims + block_dims);
             int random_l = random.Next(random_y * block_dims, random_y * block_dims + block_dims);
+
+            // Then check:
+            // - That (i, j) != (k, l)
+            // - That (i, j) or (k, l) is not a fixed coordinate
             if ((random_i == random_k && random_j == random_l) || fixed_cells.Contains(new Point(random_i, random_j)) || fixed_cells.Contains(new Point(random_k, random_l)))
-                return GetRandomCo(random_x, random_y, block_dims);
+                return GetRandomCo(random_x, random_y);
+
+            // Return i, j, k and l if these conditions hold. Otherwise recurse and select cell-coordinates
             return new List<int> { random_i, random_j, random_k, random_l };
         }
     }
